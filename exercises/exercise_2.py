eafd27c9dea338de95e6d300e5a1bcd4510ff3b8
Pierrot <128379590+PieZRwa@users.noreply.github.com>
2024-03-25 12:26:22 +0000

    Update exercise_2.py

--- exercises/exercise_2.py
+++ exercises/exercise_2.py
@@ -1,2 +1,15 @@
 # Your solution to Exercise 2
+def ageGroup(y):
+  if y<1:
+    group = "an infant."
+  elif y<13:
+    group = "a child."
+  elif y<20:
+    group = "a teenager."
+  else:
+    group = "an adult."
+  print("You are",group)
 
+ageGroup(3)
+ageGroup(14)
+ageGroup(25)
