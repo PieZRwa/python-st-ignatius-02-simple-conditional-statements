fb503f8f419f397183298ba5ef0a3b46b39acc13
Pierrot <128379590+PieZRwa@users.noreply.github.com>
2024-03-25 12:44:39 +0000

    Update exercise_4.py

--- exercises/exercise_4.py
+++ exercises/exercise_4.py
@@ -1,2 +1,13 @@
 # Your solution to Exercise 4
-
+grade = int(input("Grade = "))
+if grade in [1,2,3]:
+  level = "initial"
+elif grade in [4,5,6]:
+  level = "average"
+elif grade in [7,8,9]:
+  level = "sufficient"
+elif grade in [10,11,12]:
+  level = "high"
+else:
+  level = 'absent'
+print("Level is",level)
