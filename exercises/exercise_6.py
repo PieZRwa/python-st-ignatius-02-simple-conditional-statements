93d7fd94aa8aad3b755b10e8c96554110ee30fbb
Pierrot <128379590+PieZRwa@users.noreply.github.com>
2024-03-25 13:21:41 +0000

    Update exercise_6.py

--- exercises/exercise_6.py
+++ exercises/exercise_6.py
@@ -1,2 +1,11 @@
 # Your solution to Exercise 6
 
+def furtherfromOrigin(A1,A2,B1,B2):
+  if A1**2+A2**2 == B1**2+B2**2:
+    print("Distances are the same")
+  else:
+    print("AB"[A1**2+A2**2 < B1**2+B2**2], "is further from the origin.")
+
+furtherfromOrigin(1,2,3,4)
+furtherfromOrigin(1,2,2,4)
+furtherfromOrigin(1,2,0,0)
