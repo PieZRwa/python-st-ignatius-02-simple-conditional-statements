5006c94966d487d427d4e7a2457a6bd9498beab6
Pierrot <128379590+PieZRwa@users.noreply.github.com>
2024-03-25 12:17:55 +0000

    Update exercise_1.py

--- exercises/exercise_1.py
+++ exercises/exercise_1.py
@@ -1,2 +1,10 @@
 # Your solution to Exercise 1
+def olderOf(A,T):
+  if A==T:
+    print("Alex and Tatyana are of the same age.")
+  else:
+    print(["Alex","Tatyana",][A<T],"is the eldest.")
 
+olderOf(17,22)
+olderOf(25,25)
+olderOf(30,28)
